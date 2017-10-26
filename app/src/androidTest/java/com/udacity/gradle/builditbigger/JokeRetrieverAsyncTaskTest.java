7d5bf1abe6d6d22287d4dea79f1cc901d1c68736
thomasfischersm <thomas@playposse.com>
2017-10-26 08:01:37 -0700

    Simplify test.

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeRetrieverAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeRetrieverAsyncTaskTest.java
@@ -1,17 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
-import android.app.Activity;
-import android.app.Instrumentation;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.MediumTest;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
-
-import com.playposse.jokeactivitylibrary.JokeActivity;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,8 +12,10 @@ import org.junit.runner.RunWith;
 
 import java.util.concurrent.ExecutionException;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static junit.framework.Assert.assertNotNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.core.IsNot.not;
 
 /**
  * An instrumented test for {@link JokeRetrieverAsyncTask}.
@@ -32,17 +27,10 @@ public class JokeRetrieverAsyncTaskTest {
     private static final String LOG_TAG = JokeRetrieverAsyncTaskTest.class.getSimpleName();
 
     private Context targetContext;
-    private Instrumentation.ActivityMonitor activityMonitor;
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @Before
     public void setup() {
         targetContext = InstrumentationRegistry.getTargetContext();
-
-        // The statement below requires a higher API version than is defined for the app. That's
-        // okay because we control the environment in which this test is run.
-        activityMonitor = new Instrumentation.ActivityMonitor();
-        InstrumentationRegistry.getInstrumentation().addMonitor(activityMonitor);
     }
 
     @Test
@@ -50,18 +38,9 @@ public class JokeRetrieverAsyncTaskTest {
         JokeRetrieverAsyncTask jokeRetrieverAsyncTask = new JokeRetrieverAsyncTask(targetContext);
         jokeRetrieverAsyncTask.execute();
 
-        // Wait for the AsyncTask to complete.
-        // (Throw exception, so that JUnit can report it nicely.)
-        jokeRetrieverAsyncTask.get();
-        Activity lastActivity = activityMonitor.waitForActivity();
-
-        // Verify that the right activity got started with a joke.
-        assertEquals(JokeActivity.class, lastActivity.getClass());
-
-        // Verify the joke is present.
-        Intent lastIntent = lastActivity.getIntent();
-        String joke = lastIntent.getStringExtra(JokeActivity.JOKE_EXTRA);
+        String joke = jokeRetrieverAsyncTask.get();
+        Log.i(LOG_TAG, "retrieveRandomJoke: Got joke: " + joke);
         assertNotNull(joke);
-        Log.e(LOG_TAG, "retrieveRandomJoke: Got joke: " + joke);
+        assertThat(joke, not(isEmptyString()));
     }
 }
